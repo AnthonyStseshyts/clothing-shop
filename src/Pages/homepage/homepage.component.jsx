f7051f1b596bacf56fbc2259085e894d210d5f4a
Anthony Stseshyts <a.stseshyts@mail.ru>
2020-01-22 12:49:01 +0300

    splitted the homepage into components and added some css to the menu-item

---
+++ src/Pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Directory from "../../Components/directory";
+
+import "./homepage.component.styles.scss";
+
+const HomePage = () => (
+    <div className="homepage">
+      <Directory />
+    </div>
+  )
+;
+
+export default HomePage;
